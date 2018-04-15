b881a7ae34d5f95e38d78e772433aa8278283fa2
Andy Hang <hang.andy.t@gmail.com>
2018-04-15 16:46:17 -0500

    creating an app

---
+++ albums/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import Header from './src/components/header';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Header headerText={'Albums'} />
+      </View>
+    );
+  }
+}
+
